e293f6dc3b8e13a3adde18f6dc3009987c8d5713
RoquetaMichael96 <michael.roqueta@mykenzie.snhu.edu>
2023-01-24 19:01:40 -0800

    Added some content to start us off need to work out some bugs with getting the rectangle to move properly with key command and mouse drag.

---
+++ main/src/main/java/org/GamePlatformerLBC/GameWindow.java
@@ -0,0 +1,21 @@
+package org.GamePlatformerLBC;
+
+import javax.swing.JFrame;
+
+public class GameWindow {
+    private JFrame jFrame;
+
+    public GameWindow(GamePanel gamePanel){
+
+        jFrame = new JFrame();
+        jFrame.setSize(400,400);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.add(gamePanel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+
+
+
+
+    }
+}
